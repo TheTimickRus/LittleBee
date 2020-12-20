adfa063f7b6e71460d3f30c1153915bbedd05d0f
TheTimickRus <36606507+TheTimickRus@users.noreply.github.com>
2020-12-21 00:26:45 +0300

    upd

---
+++ src/main/java/org/exTeam/Core/Address.java
@@ -0,0 +1,40 @@
+package org.exTeam.Core;
+
+public class Address {
+    private String country;
+    private String city;
+    private String houseNumber;
+    private String apartmentNumber;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getApartmentNumber() {
+        return apartmentNumber;
+    }
+
+    public void setApartmentNumber(String apartmentNumber) {
+        this.apartmentNumber = apartmentNumber;
+    }
+}
---
+++ src/main/java/org/exTeam/Core/Products/Hive/Components/Components.java
@@ -0,0 +1,33 @@
+package org.exTeam.Core.Products.Hive.Components;
+
+import java.awt.*;
+
+public class Components {
+    private String material;
+    private String manufacturer;
+    private Color color;
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+}
